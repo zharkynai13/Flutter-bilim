5ca1ca49a2a94a5c39cd74f2adb45f027bfcb632
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-05-11 13:16:45 +0600

    add class

--- lessonsYu/dart_lessons/bin/dart_lessons.dart
+++ lessonsYu/dart_lessons/bin/dart_lessons.dart
@@ -6,18 +6,33 @@ import 'package:dart_lessons/dart_lessons.dart' as dart_lessons;
 
 // this is main function LIST use only String type
 
-void main() {
-  List <String> names = ["Aibek", 'Marat','KAnykey','Askarali'];
-  names.add("baitur");
-  names.remove("Aibek");
-  print(names);
-}
+// void main() {
+//   List <String> names = ["Aibek", 'Marat','KAnykey','Askarali'];
+//   names.add("baitur");
+//   names.remove("Aibek");
+//   print(names);
+// }
 
 // this main function use String and int types 
 
-void main1() {
-  List value =["Marat",30,'Aibek', 25, 45];
-  value.add('Ivan');
-  value.remove(45);
-  print(value);
+// void main1() {
+//   List value =["Marat",30,'Aibek', 25, 45];
+//   value.add('Ivan');
+//   value.remove(45);
+//   print(value);
+// }
+
+
+void main() {
+  User userOne = User();
+  print(userOne.name);
+  userOne.Login();
+}
+class User {
+  String name = 'Mario';
+  int age = 25;
+
+  void Login() {
+    print("object"); 
+  }
 }
