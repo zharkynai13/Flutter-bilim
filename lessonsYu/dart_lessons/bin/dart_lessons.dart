0c03018e1ca44be1e7321af1eb7e5b999d5d5d21
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-05-11 12:56:24 +0600

    add dart functon

---
+++ lessonsYu/dart_lessons/bin/dart_lessons.dart
@@ -0,0 +1,23 @@
+import 'package:dart_lessons/dart_lessons.dart' as dart_lessons;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_lessons.calculate()}!');
+// }
+
+// this is main function LIST use only String type
+
+void main() {
+  List <String> names = ["Aibek", 'Marat','KAnykey','Askarali'];
+  names.add("baitur");
+  names.remove("Aibek");
+  print(names);
+}
+
+// this main function use String and int types 
+
+void main1() {
+  List value =["Marat",30,'Aibek', 25, 45];
+  value.add('Ivan');
+  value.remove(45);
+  print(value);
+}
