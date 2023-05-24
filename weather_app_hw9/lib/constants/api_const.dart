a2ce4ed6c191bbd0c4e6f0e4d46a0d8308019cf8
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-05-24 14:35:25 +0600

    add gealocation

--- weather_app_hw9/lib/constants/api_const.dart
+++ weather_app_hw9/lib/constants/api_const.dart
@@ -1,5 +1,7 @@
 class ApiConst {
+  static String address () => 'https://api.openweathermap.org/data/2.5/onecall?lat=42.84&lon=76.06&exclude=hourly,daily,minutely&appid=41aa18abb8974c0ea27098038f6feb1b';
   static String getIcon(String IconData, int iconSize) {
     return "penweathermap.org/img/wn/$IconData@${iconSize}x.png";
   }
+  static String getWeatherData (String name) => "https://api.openweathermap.org/data/2.5/weather?q=bishkek,&appid=9d48ca0f0ec99ebd94d30163f03012be";
 }
--- weather_app_hw9/lib/view/home.dart
+++ weather_app_hw9/lib/view/home.dart
@@ -1,16 +1,92 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app_hw8/constants/api_const.dart';
 import 'package:weather_app_hw8/constants/app_color.dart';
 import 'package:weather_app_hw8/constants/app_text.dart';
-import 'package:weather_app_hw8/constants/fetch.dart';
+import 'package:dio/dio.dart';
+import 'package:weather_app_hw8/models/model.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+const List cities = <String>[
+  'Bishkek',
+  'Osh',
+  'Jalal-Abad',
+  'Karakol',
+  'Batken',
+  'Talas',
+  'Naryn'
+];
 
 class _MyHomePageState extends State<MyHomePage> {
+  Weather? weather;
+  
+  
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    } if(permission == LocationPermission.always && permission == LocationPermission.whileInUse) {
+      Position position = await Geolocator.getCurrentPosition();
+       Dio dio = Dio();
+       final res = await dio.get(ApiConst.address());
+       if(res.statusCode == 200) {
+        weather = Weather(
+       id: res.data["current"]["weather"][0]["id"],
+       main: res.data["current"]["weather"][0]["main"], 
+       description: res.data["current"]['weather'][0]['description'], 
+       icon: res.data["current"]['weather'][0]['icon'],
+       temp: res.data["current"]['temp'],
+       name: res.data["timezone"],
+     );
+   }
+     setState(() { });
 
+    }else {
+         Position position = await Geolocator.getCurrentPosition();
+       Dio dio = Dio();
+       final res = await dio.get(ApiConst.address());
+       if(res.statusCode == 200) {
+        weather = Weather(
+       id: res.data["current"]["weather"][0]["id"],
+       main: res.data["current"]["weather"][0]["main"], 
+       description: res.data["current"]['weather'][0]['description'], 
+       icon: res.data["current"]['weather'][0]['icon'],
+       temp: res.data["current"]['temp'],
+       name: res.data["timezone"],
+     );
+   }
+     setState(() { });
+    }
+
+  }
+  Future<void> fetchData(String? url) async{
+  Dio dio = Dio();
+  final res = await dio.get(ApiConst.getWeatherData(url ?? 'bishkek'));
+   if(res.statusCode == 200) {
+     weather = Weather(
+     id: res.data["weather"][0]["id"],
+     main: res.data["weather"][0]["main"], 
+     description: res.data['weather'][0]['description'], 
+     icon: res.data['weather'][0]['icon'],
+     temp: res.data['main']['temp'],
+     name: res.data['name'],
+     );
+   }
+   setState(() {
+     
+   });
+}
+ @override
+  void initState() {
+    super.initState();
+    fetchData("");
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,39 +94,30 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Home Work 9', style: AppTexts.appBarText,),
         elevation: 0,),
 
-        body: FutureBuilder(future: FetchData(),
-           builder: (context, snapshot) {
-          if(snapshot.connectionState == ConnectionState.none) {
-            return const Text("You internet is not working");
-          }else if(snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }else if(snapshot.connectionState == ConnectionState.done){
-            if(snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }else if(snapshot.hasData) {
-              final weatherSnap = snapshot.data;
-              return Center(
-                child: Container(
-                        decoration:const BoxDecoration(
-                          image: DecorationImage(image: 
-                          AssetImage("assets/bg-img.jpg"),
-                          fit: BoxFit.cover)
-                        ),
-                        child: Column(children: [
-                          Row(
+        body: Container(
+                decoration:const BoxDecoration(
+                  image: DecorationImage(image: 
+                      AssetImage("assets/bg-img.jpg"),
+                        fit: BoxFit.cover)
+                  ),
+                child: Column(children: [
+                Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const[
-                  Icon(Icons.near_me,color: AppColors.iconColor,),
-                  SizedBox(width: 190,
-                  height: 40,),
-                  Icon(Icons.location_city,color: AppColors.iconColor,)
+                children: [
+                  IconButton(onPressed: ()async{
+                    getLocation();
+                  }, icon: const Icon(Icons.near_me),color: AppColors.iconColor,iconSize: 30,),
+                  // IconButton(onPressed(){},Icons.near_me,color: AppColors.iconColor,),
+                  const SizedBox(width: 190,height: 40,),
+                   IconButton(onPressed: ()async{showBottom();}, icon: const Icon(Icons.location_city),color: AppColors.iconColor,iconSize: 30,),
                 ],
-                          ),
-                           Row(
+              ),
+              if (weather != null && weather!.temp != null)
+           Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                  children:[
                    Text(
-                    "${(weatherSnap!.temp - 273.15).toInt()}",
+                    "${(weather!.temp- 273.15).toInt()}",
                     style: AppTexts.numStyle,),
                     Image.asset("assets/clouds.png", width: 140,),
                     
@@ -58,31 +125,53 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                      Column(
                       children: [
-                        
+                         
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Text("Bishkek", style: AppTexts.cityText,),
-                            Icon(Icons.location_on,size: 40,color: Colors.red,)
+                          children:  [
+                            if (weather != null && weather!.name != null)
+                          Text("${weather!.name}", style: AppTexts.cityText,),
+                          Icon(Icons.location_on,size: 40,color: Colors.red,)
                           ],
                         ),
                         const SizedBox(height: 20,),
-                        Text("${weatherSnap.description}",style: AppTexts.descText,)
+                        if (weather != null && weather!.description != null)
+                        Text("${weather!.description}",style: AppTexts.descText,)
                         
                       ],
                     )
-                        ],),
-                      ),
-              );
-            }else {
-              return const Text("data not comming");
-            }
-          }else {
-            return const Text("null");
-          }
-        },
-        ),
-
+           ],),
+       ),
    );
   }
+   void showBottom() async{
+   showModalBottomSheet<void>(
+    context: context, 
+    builder: (BuildContext context) {
+      return Container(
+        height: 300,
+        color: Colors.amber,
+        child: ListView.builder(
+          itemCount: cities.length,
+          itemBuilder: (context, index)  {
+            final city = cities[index];
+            return  Card(
+              child: ListTile(
+                 onTap: () {
+                        setState(() {
+                          weather = null;
+                        });
+                        fetchData(city);
+                        Navigator.pop(context);
+                      },
+                title: Text("$city"),
+                ),
+            );
+          }
+        )
+        
+      );
+    }
+    );
+   }
 }
