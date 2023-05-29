e3e5132afdc51d58ff97c2f9bf4484b5d01ab96a
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-05-29 10:27:35 +0600

    do

---
+++ hw_10/lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hw_10/veiw/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
--- hw_10/test/widget_test.dart
+++ hw_10/test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hw_10/app/app.dart';
 
-import 'package:hw_10/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
