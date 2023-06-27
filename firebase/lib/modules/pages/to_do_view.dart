a250f7e6dbb0d544f731bfe701e72e20141cacf1
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-06-27 13:02:30 +0600

    to_do app

--- firebase/lib/modules/pages/to_do_view.dart
+++ firebase/lib/modules/pages/to_do_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/modules/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class ToDoView extends StatefulWidget {
@@ -9,6 +10,11 @@ class ToDoView extends StatefulWidget {
 
 class _ToDoViewState extends State<ToDoView> {
   bool isCompleted = false;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _authorController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,29 +23,78 @@ class _ToDoViewState extends State<ToDoView> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), hintText: "Text"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              maxLines: 8,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), hintText: "Description"),
-            ),
-            CheckboxListTile(
-              value: isCompleted,
-              onChanged: (v) {
-                setState(() {
-                  isCompleted = v!;
-                });
-              },
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "write title";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Text"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "write description";
+                  } else {
+                    return null;
+                  }
+                },
+                maxLines: 8,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Description"),
+              ),
+              CheckboxListTile(
+                value: isCompleted,
+                onChanged: (v) {
+                  setState(() {
+                    isCompleted = v!;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _authorController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "write author";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Author"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MyHomePage()));
+                    } else {
+                      return null;
+                    }
+                  },
+                  child: const Text("Send"))
+            ],
+          ),
         ),
       ),
     );
