2da29476b870d99840c203d840ce8b58adbb0e84
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-06-28 23:46:56 +0600

    add firebase

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:firebase/app/app.dart';
+import 'package:firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
